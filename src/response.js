a8d825e1ca83531e8e95e215a60a764e568579a6
Łukasz Ciesielski <ciesiel555@gmail.com>
2019-11-13 12:48:22 +0100

    Refactore code in Response

--- src/response.js
+++ src/response.js
@@ -1,9 +1,14 @@
 const ALLOWED_STATUSES = ['success', 'fail', 'error'];
+const DEFAULT_STATUS_CODE = {
+  SUCCESS: 200,
+  FAIL: 400,
+  ERROR: 500
+}
 
 var Response = function (data) {
-  checkInputData(data);
+  this.status = getStatus(data);
+  setStatusCode.call(this, data)
   this.payload = createPayload(data);
-  this.statusCode = data.statusCode || getDefaultStatusCode.call(this);
 }
 
 Response.prototype.jsend = function (res) {
@@ -16,17 +21,53 @@ Response.prototype.withData = function (data) {
   return this;
 }
 
-var checkInputData = function (data) {
+var getStatus = function (data) {
   if (!data) throw new Error('Input data not found. Fill in your API model.');
   if (!data.status) throw new Error('Status field is required in model.');
   if (!ALLOWED_STATUSES.includes(data.status)) throw new Error('Invalid status.');
-  if ((data.status === 'error') && !data.message) throw new Error('Message field is required for error response');
+  return data.status;
+}
+
+var setStatusCode = function (data) {
+  this.statusCode = data.statusCode || getDefaultStatusCode.call(this);
+}
+
+var getDefaultStatusCode = function () {
+  let status = this.status.toUpperCase();
+  return DEFAULT_STATUS_CODE[status];
 }
 
 var createPayload = function (data) {
+  switch (data.status) {
+    case 'success':
+      return getSuccessPayload(data);
+    case 'fail':
+      return getFailPayload(data);
+    case 'error':
+      return getErrorPayload(data);
+  }
+}
+
+var getSuccessPayload = function (data) {
   let payload = {};
   payload.status = data.status;
   if (data.message) payload.message = data.message;
+  return payload;
+}
+
+var getFailPayload = function (data) {
+  let payload = {};
+  payload.status = data.status;
+  if (data.message) payload.message = data.message;
+  return payload;
+}
+
+var getErrorPayload = function (data) {
+  let payload = {};
+  payload.status = data.status;
+  if (!data.message) throw new Error('Message field is required for error response');
+  payload.message = data.message;
+  if (data.code) payload.code = data.code;
   return payload;
 }
 
@@ -35,13 +76,6 @@ var fillDataFieldWithNullForSuccessAndFail = function () {
   let hasProperStatus = (status === 'success' || status === 'fail');
   let dataFieldNotExist = !this.payload.data; 
   if ( hasProperStatus && dataFieldNotExist ) this.payload.data = null;
-}
-
-var getDefaultStatusCode = function () {
-  let status = this.payload.status;
-  if (status === 'success') return 200;
-  if (status === 'fail') return 400;
-  return 500;
 }
 
 module.exports = Response;
